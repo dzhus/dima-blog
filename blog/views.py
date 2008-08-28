5b96510c512130f393e7f364182de7bbe7aaa6b4
Dmitry Dzhus <dima@dzhus.org>
2008-08-28 23:05:37 +0400

    Links to previous/next entry in entry detail view are now working (dirty hack involved).

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from django.core.paginator import Paginator, InvalidPage
 
 from django.http import HttpResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
 from django.template.loader import get_template
 
@@ -16,21 +17,21 @@ from blog.models import Entry
 # from md5 import md5 as md5hash
 # from random import shuffle
 
-def filter_inappropriate(request, queryset):
+def make_filter_kwargs(request):
     """
     Take queryset and filter out objects inappropriate for current
     user (e. g. private entries shouldn't be seen by guests).
     """
     if not (request.user.is_authenticated() and \
             request.user.has_perm('blog.can_see_private')):
-        return queryset.filter(private=0)
+        return {'private': 0}
     else:
-        return queryset    
+        return {}
 
 def entry_list(request, tag=None, page='last', **kwargs):
     objects = Entry.objects
 
-    queryset = filter_inappropriate(request, objects.order_by('add_date'))
+    queryset = objects.order_by('add_date').filter(**make_filter_kwargs(request))
 
     return object_list_view(request, queryset,
                             extra_context={'tag': tag,
@@ -39,10 +40,36 @@ def entry_list(request, tag=None, page='last', **kwargs):
                             **kwargs)
         
 def entry_detail(request, queryset, **kwargs):
-    queryset = filter_inappropriate(request, queryset)
+    filter_kwargs = make_filter_kwargs(request)
+    queryset = queryset.filter(**filter_kwargs)
+
+    # Violate DRY badly as generic detail view doesn't allow to add
+    # extra context which uses found object
+    if kwargs['object_id']:
+        queryset = queryset.filter(pk=kwargs['object_id'])
+    elif kwargs['slug']:
+        queryset = queryset.filter(slug=kwargs['slug'])
+    try:
+        obj = queryset.get()
+    except ObjectDoesNotExist:
+        raise Http404
+
+    # Exception not necessarily happen at the same time, so use two
+    # try..except blocks
+    try:
+        prev_entry = obj.get_previous_by_add_date(**filter_kwargs)
+    except ObjectDoesNotExist:
+        prev_entry = None
+    try:
+        next_entry = obj.get_next_by_add_date(**filter_kwargs)
+    except ObjectDoesNotExit:
+        next_entry = None
     
     return object_detail_view(request, queryset,
-                              extra_context={'full_view': True},
+                              extra_context={'full_view': True,
+                                             'filter': filter_kwargs,
+                                             'prev_entry': prev_entry,
+                                             'next_entry': next_entry},
                               **kwargs)
 
 def tag_list(request, template_name="entry_detail.xhtml"):
