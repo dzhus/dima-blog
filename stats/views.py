a087ec7603a88cc78592a9a8830749e8afd7f07f
Dmitry Dzhus <dima@dzhus.org>
2009-01-06 21:32:24 +0300

    Pie charts added

--- stats/views.py
+++ stats/views.py
@@ -4,6 +4,7 @@ import time
 import datetime
 
 from pygooglechart import XYLineChart
+from pygooglechart import PieChart2D
 from pygooglechart import Axis
 
 from django.shortcuts import render_to_response
@@ -76,6 +77,69 @@ def make_pool_uniform_chart(object_list, width, height,
 
     return chart
 
+# TODO Use implementation from standard django template tags
+def group_by_year(object_list, date_field='add_date', ):
+    """
+    Return dictionary of objects grouped by years
+    """
+    groups = dict()
+    for obj in object_list:
+        key = obj.__dict__[date_field].year
+        if key not in groups:
+            groups[key] = list()
+        groups[key].append(obj)
+    return groups
+
+def make_piechart(groups, width, height,
+                  labels=True, colors=None):
+    """
+    Make piechart from dictionary.
+
+    Dictionary items set pie chart data, while dictionary keys are
+    used for labels.
+    """
+    chart = PieChart2D(width, height)
+
+    chart.add_data(groups.values())
+
+    if labels:
+        chart.set_pie_labels(map(str, groups.iterkeys()))
+
+    if not colors is None:
+        chart.set_colours(colors)
+
+    return chart
+
+def dict_transform_values(orig_dict, transform):
+    """
+    Apply `transform` to every value of `orig_dict` and return new
+    dictionary.
+
+    Keys are unchanged.
+    
+    >>> dict_transform_values({'a': 5.1, 'b': 6.31}, int)
+    {'a': 5, 'b': 26}
+    """
+    new_dict = dict()
+    for (k, i) in orig_dict.items():
+        new_dict[k] = transform(i)
+    return new_dict
+
+def make_q_piechart(object_list, width, height,
+                    labels=True, colors=None):
+    pie_dict = dict_transform_values(group_by_year(object_list), len)
+    chart = make_piechart(pie_dict, width, height)
+    
+    return chart
+
+def make_l_piechart(object_list, width, height,
+                    labels=True, colors=None):
+    pie_dict = dict_transform_values(group_by_year(object_list),
+                                     lambda objects: sum(map(lambda object: len(object), objects)))
+    chart = make_piechart(pie_dict, width, height)
+
+    return chart
+        
 def queryset_stats(request, queryset, template_name,
                    width=600, height=300, color=None):
     entry_list = queryset.filter(**make_filter_kwargs(request))
@@ -85,6 +149,10 @@ def queryset_stats(request, queryset, template_name,
     uni_chart = make_pool_uniform_chart(entry_list, width, height, color=color)
 
     context = {'date_chart': date_chart,
-               'uni_chart': uni_chart}
+               'uni_chart': uni_chart,
+               'uni_pie': make_q_piechart(entry_list, width=400, height=250),
+               # How cute, a date pie :-)
+               'date_pie': make_l_piechart(entry_list, width=400, height=250)}
     
     return render_to_response(template_name, context)
+
