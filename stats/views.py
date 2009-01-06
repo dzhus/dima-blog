962bfc8774bfe260968fb7c8cebb3183269283fa
Dmitry Dzhus <dima@dzhus.org>
2009-01-06 18:18:12 +0300

    Moved labeling and coloring to `make_pool_*` functions

--- stats/views.py
+++ stats/views.py
@@ -29,7 +29,8 @@ def make_pool_chart(object_list, width, height, x_values):
     
     return chart
 
-def make_pool_date_chart(object_list, width, height, date_field='add_date'):
+def make_pool_date_chart(object_list, width, height, date_field='add_date',
+                         labels=True, label_format='%Y-%m', color=None):
     """
     Return a chart of probability function for given objects with
     length and datetime field named as specified in `date_field`
@@ -38,46 +39,57 @@ def make_pool_date_chart(object_list, width, height, date_field='add_date'):
     `width` and `height` are integers with their multiple being less
     than 300000.
     """
-    return make_pool_chart(object_list, width, height,
-                           map(lambda o: date_to_epoch(o.__dict__[date_field]),
-                               object_list))
+    def make_date_label(date):
+        return date.strftime(label_format)
+    
+    chart =  make_pool_chart(object_list, width, height,
+                             map(lambda o: date_to_epoch(o.__dict__[date_field]),
+                                 object_list))
 
-def make_pool_uniform_chart(object_list, width, height):
+    if labels:
+        first_object = object_list[0]
+        last_object = object_list.reverse()[0]
+        
+        first_date = first_object.add_date
+        last_date = last_object.add_date
+        
+        chart.set_axis_labels(Axis.LEFT, ['', chart.data[1][-1:][0]])
+        chart.set_axis_labels(Axis.BOTTOM, map(make_date_label,
+                                               [first_date, last_date]))
+        chart.set_axis_labels(Axis.BOTTOM, ['1', len(object_list)])
+    
+    if not color is None:
+        chart.set_colours([color])
+
+    return chart
+
+def make_pool_uniform_chart(object_list, width, height,
+                            labels=True, color=None):
     """
     The same as `make_pool_date_chart`, but without any regard to
     objects creation date, so that pool sizes are uniformly
     distributed along *X* axis.
     """
-    return make_pool_chart(object_list, width, height,
-                           range(len(object_list)))
+    chart =  make_pool_chart(object_list, width, height,
+                             range(len(object_list)))
+
+    if labels:
+        chart.set_axis_labels(Axis.LEFT, ['', chart.data[1][-1:][0]])
+        chart.set_axis_labels(Axis.BOTTOM, ['1', len(object_list)/2, len(object_list)])
+    
+    if not color is None:
+        chart.set_colours([color])
+
+    return chart
 
 def queryset_stats(request, queryset, template_name,
-                   width=600, height=300,
-                   label_format="%Y-%m", color=None):
-    def make_label(date):
-        return date.strftime(label_format)
-    
+                   width=600, height=300, color=None):
     entry_list = queryset.filter(**make_filter_kwargs(request))
 
     # Create probability chart
-    date_chart = make_pool_date_chart(entry_list, width, height)
-    uni_chart = make_pool_uniform_chart(entry_list, width, height)
+    date_chart = make_pool_date_chart(entry_list, width, height, color=color)
+    uni_chart = make_pool_uniform_chart(entry_list, width, height, color=color)
 
-    if not color is None:
-        date_chart.set_colours([color])
-        uni_chart.set_colours([color])
-
-    # Set chart labels
-
-    first_date = entry_list[0].add_date
-    last_date = entry_list.reverse()[0].add_date
-    
-    date_chart.set_axis_labels(Axis.LEFT, ['', date_chart.data[1][-1:][0]])
-    date_chart.set_axis_labels(Axis.BOTTOM, map(make_label,
-                                           [first_date, last_date]))
-    uni_chart.set_axis_labels(Axis.LEFT, ['', date_chart.data[1][-1:][0]])
-    uni_chart.set_axis_labels(Axis.BOTTOM, ['1', len(queryset)])
-    
     context = {'date_chart': date_chart,
                'uni_chart': uni_chart}
     
