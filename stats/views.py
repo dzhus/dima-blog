51ad7a8bae0d1231501f1124ff5af661a8251f14
Dmitry Dzhus <dima@dzhus.org>
2009-01-07 01:43:17 +0300

    Documented `queryset_stats` view and removed filtering from it, renamed context names

--- stats/views.py
+++ stats/views.py
@@ -9,17 +9,37 @@ from chart_helpers import make_q_piechart, make_l_piechart
         
 def queryset_stats(request, queryset, template_name,
                    width=600, height=300, color=None):
-    entry_list = queryset.filter(**make_filter_kwargs(request))
+    """
+    Provide charts with statistics for objects in `queryset`.
 
+    `color` is a string in ``RRGGBB`` or ``RRGGBBAA`` form which sets
+    color for object pool graphs. `colors` (list of strings like in
+    `color`) sets colors used for pie charts.
+
+    Integer `width` and `height` are chart image dimensions.
+    
+    Context:
+        uni_chart
+            Object pool size graph with object numbers used for X axis values
+        date_chart
+            Object pool size graph with creation dates used for X
+        quantity_pie
+            Pie chart with amount of objects created each year
+        measure_pie
+            Pie chart with total measures of objects created each year
+
+    To get image URLs for charts, `get_url` method should be used in
+    the template (like `uni_chart.get_url`), as well as their `width`
+    and `height` fields.
+    """
     # Create probability chart
-    date_chart = make_pool_date_chart(entry_list, width, height, color=color)
-    uni_chart = make_pool_uniform_chart(entry_list, width, height, color=color)
+    date_chart = make_pool_date_chart(queryset, width, height, color=color)
+    uni_chart = make_pool_uniform_chart(queryset, width, height, color=color)
 
     context = {'date_chart': date_chart,
                'uni_chart': uni_chart,
-               'uni_pie': make_q_piechart(entry_list, width, height),
-               # How cute, a date pie :-)
-               'date_pie': make_l_piechart(entry_list, width, height)}
+               'quantity_pie': make_q_piechart(queryset, width, height),
+               'measure_pie': make_l_piechart(queryset, width, height)}
     
     return render_to_response(template_name, context)
 
