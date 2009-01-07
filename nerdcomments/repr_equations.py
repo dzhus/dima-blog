686194935950fa1c150b4b48bed8a9936284e7e8
Dmitry Dzhus <dima@dzhus.org>
2009-01-07 23:33:36 +0300

    Added lots of comments ro `repr_equations.py`, fixed few bugs

--- nerdcomments/repr_equations.py
+++ nerdcomments/repr_equations.py
@@ -1,16 +1,60 @@
+# -*- coding: utf-8 -*-
+# Description
+# ===========
+#
+# This module introduces several classes aimed to provide means of
+# constructing, evaluating and printing of arbitary (mathematical)
+# expressions:
+#
+# - `ReprFunction`
+#
+# - `ReprArgument`
+#
+# - `ReprExpression`
+#
+# - `ReprEquation`
+#
+# Why Do I Use Infix Notation
+# ===========================
+#
+# KISS! This is merely a helper module to construct CAPTCHAs :-)
+#
+# List of things to be done
+# ========================
+#
+# - Warn about wrong number of conversion specifiers in
+#   `ReprFunction()` and `ReprExpression()`
+#
+# Examples
+# ========
+#
+# You may want to read doctest examples in `repr_functions.py` to
+# understand how this module works. See real-world example in
+# `nerdcaptcha.py`.
+
 import inspect
 
-def reduce_by_predicate(iterable, predicate):
+def true_for_all(iterable, predicate):
     """
     Test if predicate is true for all items of iterable.
+    
+    >>> true_for_all([1, 3, 1771], lambda x: x % 2 == 1)
+    True
     """
     return reduce(lambda x, y: x and predicate(y), iterable, True)
 
 def all_are_instances(iterable, cl):
     """
     Test if all elements of iterable are instances of cl.
+    
+    >>> all_are_instances([1, 2, 3], int)
+    True
+    >>> all_are_instances([-5, -6, 'foo'], int)
+    False
+    >>> all_are_instances([ValueError(), AssertionError()], BaseException)
+    True
     """
-    return reduce_by_predicate(iterable, lambda x: isinstance(x, cl))
+    return true_for_all(iterable, lambda x: isinstance(x, cl))
 
 class ExpressionPart:
     pass
@@ -20,11 +64,65 @@ class Repr(str, ExpressionPart):
         return str.__new__(cl, repr)
     
 class ReprFunction(Repr):
+    
     """
-    A class for functions which also have human-readable
-    representation
+    Loosely mimics functions and strings.
+
+    Instances of this class are primarly used when constructin
+    `ReprExpression` or `ReprEquation` objects.
     """
+    
     def __init__(self, repr, fun, arity=None, priority=0):
+        """
+        Construct a new ReprFunction instance which is printed using
+        `repr` string and acts like `fun` function when applied to
+        argument(s).
+        
+        `repr` must contain ``%s`` conversion specifiers in amount
+        equal to function arity, which is specified by `arity` integer
+        argument.
+
+        Variadic functions are not implemented.
+
+        If `arity` is ``None``, then an attempt to get arity
+        information via introspection is performed using `inspect`
+        module.
+
+        >>> r = ReprFunction('(add1 %s)', lambda x: x + 1)
+        >>> # Hey, your parentheses are wrong. Wait… OH SHI~
+        >>> r(5)
+        6
+        >>> print r % -100
+        (add1 -100)
+        
+        You have to manually set arity for builtins, because `inspect`
+        can't handle them.
+        
+        >>> r = ReprFunction('[%s]', round, 1)
+        >>> print r
+        [%s]
+        >>> r(19.89)
+        20.0
+        >>> r(31.337) == round(31.337)
+        True
+        >>> print r % 31.337
+        [31.337]
+
+        >>> p = ReprFunction('%s^%s', pow, 2)
+        >>> print p
+        %s^%s
+        >>> p(2, 5) == pow(2, 5)
+        True
+        >>> p(3, 3)
+        27
+        >>> print p % (4, 2)
+        4^2
+    
+        `priority` is an integer number used only to properly *print*
+        nested function calls in `ReprExpression` class. You'll need
+        to properly set this option for binary operations, so that
+        division has higher priority than addition etc.
+        """
         if not callable(fun):
             raise TypeError('Function must be callable')
         else:
@@ -32,12 +130,12 @@ class ReprFunction(Repr):
 
         if arity is None:
             try:
-                arity = len(inspect.getargspec(c)[0])
+                arity = len(inspect.getargspec(fun)[0])
             except:
                 raise StandardError('Arity information is unavailable')
 
         if not isinstance(arity, int):
-            raise TypeError('Arity should be integer number')
+            raise TypeError('Arity must be integer')
         else:
             self.arity = arity
 
@@ -47,10 +145,35 @@ class ReprFunction(Repr):
         return self._function(*args, **keywords)
 
 class ReprArgument(ReprFunction):
+    
+    """
+    Constant flavor of ReprFunction.
+
+    *Call* it with no arguments to get its value.
+
+    Instances of this class are primarly used when constructin
+    `ReprExpression` or `ReprEquation` objects.
+    """
+    
     def __new__(cl, repr, value):
         return ReprFunction.__new__(cl, repr)
 
     def __init__(self, repr, value):
+        """
+        Construct new ReprArgument instance.
+        
+        `repr` is a string with no conversion specifiers used for
+        printing. `value` is any valid value this function evaluates
+        to.
+
+        >>> f = ReprArgument('four', 4)
+        >>> print f
+        four
+        >>> f
+        'four'
+        >>> f()
+        4
+        """
         self._function = lambda: value
         self.arity = self.priority = 0
 
@@ -61,9 +184,14 @@ class ExpressionTypeError(StandardError):
     pass
 
 class ReprExpression():
+    
     """
-    Mathematical expression which knows how to print and eval itself.
+    Mathematical expression which can be printed and evaluated.
+
+    To get `ReprExpression` value, call it with no arguments. Use
+    standard ways of printing, like `print`.
     """
+    
     def __traverse_expression(self, items, wrapper):
         """
         Form an expression using given items.
@@ -71,19 +199,18 @@ class ReprExpression():
         Nested function calls are transformed into nested Expression
         subinstances.
         """
-        if not all_are_instances(items, ExpressionPart):
-            raise ExpressionTypeError('All items items must be ExpressionPart instances')
-
         head = items[0]
         
         if isinstance(head, ReprFunction):
             # Expression is a single function application
+            # (case: [+, 5, 5])
             if all_are_instances(items[1:], ReprArgument):
                 if len(items) == head.arity + 1:
                     return items
                 else:
                     raise ExpressionArityError('Wrong argument count')
             # Expression contains nested function calls
+            # (case: [+, *, 5, 3, 2])
             else:
                 try:
                     res = [head]
@@ -92,6 +219,7 @@ class ReprExpression():
                                               wrapper, head.priority))
                     res += items[(len(items) - head.arity + 1):]
                     return res
+                # Case: [+, *, 5, 3]
                 except IndexError:
                     raise ExpressionArity('Not enough arguments for %s' % head)
         # Expression is a single argument
@@ -101,6 +229,27 @@ class ReprExpression():
             raise ExpressionTypeError('Unknown ExpressionPart')
     
     def __init__(self, items, wrapper='(%s)', enclosing_priority=0):
+        """
+        Construct new ReprExpression from `items`.
+
+        `items` must be a list of `ExpressionPart` instances. `items`
+        ordering must follow standard infix notation with
+        `ReprFunction` instances coming first, being followed by
+        matching number of `ReprArgument` objects. If arity of
+        functions does not match, `ExpressionArityError` exception is
+        raised.
+
+        `wrapper` is a string with one ``%s`` specifier which used to
+        format expression when it occurs within another one which has
+        higher priority, given by `enclosing_priority`. This string is
+        set to ``(%s)`` by default, but you may change it.
+
+        Setting `wrapper` and `enclosing_priority` is not needed to
+        create new `ReprExpression` class.
+        """
+        if not all_are_instances(items, ExpressionPart):
+            raise ExpressionTypeError('All items must be ExpressionPart instances')
+        
         self.__expr = self.__traverse_expression(items, wrapper=wrapper)
 
         head = items[0]
@@ -111,25 +260,45 @@ class ReprExpression():
         return self.__expr[key]
     
     def __call__(self):
-        """
-        Evaluate expression.
-        """
+        """Evaluate expression."""
         return self[0](*map(apply, self[1:]))
 
     def __str__(self):
-        """
-        Print expression in infix form.
-        """
+        """Print expression in *infix* form."""
         return self.__str_wrapper % (str(self[0]) % tuple(map(str, self[1:])))
     
 class ReprEquation():
+    
     """
-    Wrap Expression
+    `ReprEquation` is a thin wrapper for `ReprExpression`, which uses
+    both expression and its *value* when printing.
     """
+    
     def __init__(self, items, priority_wrapper='(%s)', equal_wrapper='%s=%s'):
+        """
+        Constructs `ReprEquation` instance using `items` the same way
+        `ReprExpression` class constructor does.
+
+        `priority_wrapper` has the same meaning as in
+        `ReprExpression`.
+
+        `equal_wrapper` is a string with two ``%s`` specifiers
+        substituted for expression and its value when printing.
+
+        >>> a = ReprFunction('absolute value of %s', abs, 1)
+        >>> f = ReprArgument('minus four', -4)
+        >>> print ReprEquation([a, f])
+        absolute value of minus four=4
+        >>> print ReprEquation([a, f], equal_wrapper='%s is actually equal to %s')
+        absolute value of minus four is actually equal to 4
+        """
         self.__expression = ReprExpression(items, wrapper=priority_wrapper)
         self.__lvalue = self.__expression()
         self.equal_wrapper = equal_wrapper
 
     def __str__(self):
         return self.equal_wrapper % (str(self.__expression), str(self.__lvalue))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
