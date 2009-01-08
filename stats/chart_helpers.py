f907703442fd36a8a6c205f0812ed516b6368c5a
Dmitry Dzhus <dima@dzhus.org>
2009-01-08 11:52:11 +0300

    Reorganized stats code

--- stats/chart_helpers.py
+++ stats/chart_helpers.py
@@ -5,6 +5,30 @@ Helper functions used in `views.py`
 import time
 import datetime
 
+def fit_to_unit(data):
+    """
+    Shift and rescale `data` to fit unit interval.
+
+    >>> rescale_x([5, 10, 15])
+    [0.0, 0.5, 1.0]
+    """
+    head = data[0]
+    data = map(lambda x: x - head, data)
+    tail = data[-1:][0]
+    return map(lambda x: x/float(tail), data)
+
+
+def make_year_labels(years, yearly_data, format='%d (%s)'):
+    """
+    Combine years with respective yearly data and return list of labels.
+
+    >>> make_year_labels([2005, 2006], [18, 29])
+    ['2005 (18)', '2006 (29)']
+    >>> make_year_labels([2006, 2007], ['good', 'bad'], '%d was %s')
+    ['2006 was good', '2007 was bad']
+    """
+    return [format % (y, c) for (y, c) in zip(years, yearly_data)]
+
 def date_to_epoch(date):
     """
     Convert datetime to Epoch seconds.
--- stats/views.py
+++ stats/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render_to_response
 
 from prob_utils import pool_size_function
-from chart_helpers import date_to_epoch, group_by_year
+from chart_helpers import date_to_epoch, group_by_year, make_year_labels, fit_to_unit
 
 def queryset_stats(request, queryset, template_name, date_field='add_date'):
     """
@@ -16,30 +16,23 @@ def queryset_stats(request, queryset, template_name, date_field='add_date'):
             Creation dates of pool objects, rescaled
         yearly_count
             List with numbers of objects created each year
+        yc_labels
+            Labels for yearly object count chart
         yearly_measure
             List with overall measures of objects created each year
-
-    To get image URLs for charts, `get_url` method should be used in
-    the template (like `uni_chart.get_url`), as well as their `width`
-    and `height` fields.
+        ym_labels
+            Labels for yearly objects count chart
     """
-    def rescale_x(data):
-        "Shift and rescale `data` to fit unit interval."
-        head = data[0]
-        data = map(lambda x: x - head, data)
-        tail = data[-1:][0]
-        return map(lambda x: x/float(tail), data)
 
     count = queryset.count()
 
-    # Rescale pool size data
+    # Rescale pool size data (can't rely on Google Charts)
     pool_data = [x for x in pool_size_function(queryset)]
     pool_data = map(lambda x: float(x)/pool_data[-1:][0], pool_data)
 
-
-    pool_x_range = rescale_x(range(count))
-    pool_x_dates = rescale_x(map(lambda o: date_to_epoch(o.__dict__[date_field]),
-                                 queryset))
+    pool_x_range = fit_to_unit(range(count))
+    pool_x_dates = fit_to_unit(map(lambda o: date_to_epoch(o.__dict__[date_field]),
+                                   queryset))
 
     year_groups = group_by_year(queryset, date_field)
     years = year_groups.keys()
@@ -51,18 +44,16 @@ def queryset_stats(request, queryset, template_name, date_field='add_date'):
     yearly_count = map(len, yearly_objects)
     yearly_measure = map(lambda year: sum(map(lambda object: len(object), year)), yearly_objects)
 
-    yc_labels = ['%d (%s)' % (y, c) for (c, y) in zip(yearly_count, years)]
-    ym_labels = ['%d (%s)' % (y, c) for (c, y) in zip(yearly_measure, years)]
+    yc_labels = make_year_labels(years, yearly_count)
+    ym_labels = make_year_labels(years, yearly_measure)
 
-    group_by_year(queryset, date_field).values()
-
-    context = { 'pool_data': pool_data,
-                'pool_x_range': pool_x_range,
-                'pool_x_dates': pool_x_dates,
-                'yearly_count': yearly_count,
-                'yearly_measure': yearly_measure,
-                'yc_labels': yc_labels,
-                'ym_labels': ym_labels}
+    context = dict([(x, locals()[x]) for x in ['pool_data',
+                                               'pool_x_range',
+                                               'pool_x_dates',
+                                               'yearly_count',
+                                               'yearly_measure',
+                                               'yc_labels',
+                                               'ym_labels']])
     
     return render_to_response(template_name, context)
 
